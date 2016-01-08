0d33caa4866c22d3e0e6f8865cb9c70f0ef1dd39
Matthew Conlen <mc@mathisonian.com>
2016-01-07 21:59:44 -0500

    semistandard

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
+var fs = require('fs');
 var render = require('@fivethirtyeight/d3-pre-renderer');
 
 var args = process.argv.slice(2);
 
-if(!args.length) {
+if (!args.length) {
   console.log('must supply an html file');
 }
 
-
-render({inputFile: args[0]}, function(err, output) {
-  if(err) {
+render({inputFile: args[0]}, function (err, output) {
+  if (err) {
     console.log(err);
     return;
   }
 
-
-  fs.writeFile(args[0], output, function(err) {
-    if(err) {
-      console.log('error saving document', err)
+  fs.writeFile(args[0], output, function (err) {
+    if (err) {
+      console.log('error saving document', err);
     } else {
-      console.log('The file was saved!')
+      console.log('The file was saved!');
     }
   });
-
 });
